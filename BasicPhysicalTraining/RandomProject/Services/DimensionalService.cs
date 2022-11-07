7a3e52f12dcc4798063caf6dd98b4b640e652fc8
robo <robolab1902@naver.com>
2022-11-07 22:59:59 +0900

    다차원 배열 알고리즘 수정

--- BasicPhysicalTraining/RandomProject/Services/DimensionalService.cs
+++ BasicPhysicalTraining/RandomProject/Services/DimensionalService.cs
@@ -32,48 +32,47 @@ namespace RandomProject.Services
         public void setZeroToSix()
         {
             int[,] array = new int[4, 4];
-            int[] deck = new int[16];
 
-            for (int i = 0; i < deck.Length; i++)
+            int zeroCount = 0;
+            for(int i=0; i<array.GetLength(0); i++)
             {
-                deck[i] = i;
-            }
-
-            shuffle(deck);
-
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
+                for(int j=0; j<array.GetLength(1); j++)
                 {
-                    array[i, j] = random.Next(1, 11);
+                    if(zeroCount >= 6)
+                    {
+                        array[i, j] = random.Next(1, 11);
+                    }
+                    zeroCount++;
                 }
             }
 
-            for (int i = 0; i <deck.Length; i++)
-            {
-                array[deck[i]] = 0;
-            }
+            shuffle(array);
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.Write("{0} ", array[i, j]);
+                    if (zeroCount >= 6)
+                    {
+                        Console.Write("{0} ", array[i, j]);
+                    }
                 }
                 Console.WriteLine();
             }
         }
 
-        public void shuffle(int[] deck)
+        public void shuffle(int[,] deck)
         {
-            for (int i = 0; i < deck.Length; i++)
+            for (int i = 0; i < 16; i++)
             {
-                int rand = random.Next(0, 100);
-                int rand2 = random.Next(0, 100);
+                int rand = random.Next(0, 4);
+                int rand2 = random.Next(0, 4);
+                int rand3 = random.Next(0, 4);
+                int rand4 = random.Next(0, 4);
 
-                int temp = deck[rand];
-                deck[rand] = deck[rand2];
-                deck[rand2] = temp;
+                int temp = deck[rand,rand2];
+                deck[rand, rand2] = deck[rand3, rand4];
+                deck[rand3, rand4] = temp;
             }
         }
     }
