084e617d3cf2b9b673df2b3e2d649684e91708a4
JuYoung Kim <robolab1902@naver.com>
2023-03-13 15:32:12 +0900

    백준 1504 번 푸는 중

--- CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
+++ CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
@@ -1,48 +1,93 @@
-#include "pch.h"
-#include <cstdlib>
-#include <ctime>
-//#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
 
-void Print(const vector<int>& arr)
+struct vertex {
+	int n;
+	int w;
+};
+
+vector<vector<vertex>> linkList;
+vector<bool> visit;
+vector<int> distances;
+
+void explorer(const int arrive, const int before)
 {
-	for (int i : arr)
+	const vector<vertex>& links = linkList[arrive];
+
+	auto cmp = [](const vertex& v1, const vertex& v2) {
+		return (v1.n > v2.n);
+	};
+
+	priority_queue < vertex, vector<vertex>, decltype(cmp)> pq(cmp);
+
+	for (int i = 0; i < links.size(); i++)
 	{
-		cout << i << ", ";
+		const vertex& v = links[i];
+		if (visit[v.n] == true)
+			continue;
+
+		if (distances[arrive] > v.w + before)
+			distances[arrive] = v.w + before;
+
+		if (linkList[v.n].empty() == false)
+		{
+			pq.push({ v.n, distances[arrive] });
+		}
+			
+	}
+
+	if (pq.size() != 0)
+	{
+		const vertex& v = pq.top();
+		visit[v.n] = true;
+		explorer(v.n, v.w);
 	}
 }
 
 int main()
 {
-	// 백준 필요 cin
-	//ios_base::sync_with_stdio(false);
-	//cin.tie(nullptr);
-	//cout.tie(nullptr);
-	vector<vector<int>> times;
-	int n = 5;
-	int k = 1;
+	int n, e, start, end;
+	//cin >> n >> e;
+	n = 4;
+	e = 6;
+	start = 2;
+	end = 3;
 
-	// 기존 알고리즘으로 틀린 부분.
-	times.push_back({2,4,10});
-	times.push_back({5,2,38});
-	times.push_back({3,4,33});
-	times.push_back({4,2,76});
-	times.push_back({3,2,64});
-	times.push_back({1,5,54});
-	times.push_back({1,4,98});
-	times.push_back({2,3,61});
-	times.push_back({2,1,0});
-	times.push_back({3,5,77});
-	times.push_back({5,1,34});
-	times.push_back({3,1,79});
-	times.push_back({5,3,2});
-	times.push_back({1,2,59});
-	times.push_back({4,3,46});
-	times.push_back({5,4,44});
-	times.push_back({2,5,89});
-	times.push_back({4,5,21});
-	times.push_back({1,3,86});
-	times.push_back({4,1,95});
+	linkList.resize(n + 1);
+	visit.resize(n + 1);
+	distances.resize(n + 1);
 
-	DelayNetwork d;
-	cout << d.networkDelayTime(times, n, k) << endl;
+	for (int i = 1; i < n + 1; i++)
+		distances[i] = INT_MAX;
+
+	/*for (int i = 0; i < e; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		linkList[a].push_back({ b,c });
+	}
+	cin >> start >> end;*/
+	linkList[1].push_back({ 2,3 });
+	linkList[2].push_back({ 1,3 });
+	linkList[2].push_back({ 3,3 });
+	linkList[3].push_back({ 2,3 });
+	linkList[3].push_back({ 4,1 });
+	linkList[4].push_back({ 3,1 });
+	linkList[1].push_back({ 3,5 });
+	linkList[3].push_back({ 1,5 });
+	linkList[2].push_back({ 4,5 });
+	linkList[4].push_back({ 2,5 });
+	linkList[1].push_back({ 4,4 });
+	linkList[4].push_back({ 1,4 });
+
+	visit[start] = true;
+	explorer(start, 0);
+
+	if (visit[end] == true)
+		cout << distances[end];
+	else
+		cout << -1;
 }
