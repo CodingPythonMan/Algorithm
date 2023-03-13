7143be8d5d36b27d5a6e364af61a20e134d219d0
JuYoung Kim <robolab1902@naver.com>
2023-03-13 17:20:49 +0900

    백준 풀이 올리기

--- CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
+++ CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
@@ -13,12 +13,21 @@ vector<vector<vertex>> linkList;
 vector<bool> visit;
 vector<int> distances;
 
+void init(int n)
+{
+	for (int i = 1; i < n + 1; i++)
+		distances[i] = INT_MAX;
+
+	for (int i = 1; i < n + 1; i++)
+		visit[i] = false;
+}
+
 void explorer(const int arrive, const int before)
 {
 	const vector<vertex>& links = linkList[arrive];
 
 	auto cmp = [](const vertex& v1, const vertex& v2) {
-		return (v1.n > v2.n);
+		return (v1.w > v2.w);
 	};
 
 	priority_queue < vertex, vector<vertex>, decltype(cmp)> pq(cmp);
@@ -29,12 +38,12 @@ void explorer(const int arrive, const int before)
 		if (visit[v.n] == true)
 			continue;
 
-		if (distances[arrive] > v.w + before)
-			distances[arrive] = v.w + before;
+		if (distances[v.n] > v.w + before)
+			distances[v.n] = v.w + before;
 
 		if (linkList[v.n].empty() == false)
 		{
-			pq.push({ v.n, distances[arrive] });
+			pq.push({ v.n, distances[v.n] });
 		}
 			
 	}
@@ -49,19 +58,19 @@ void explorer(const int arrive, const int before)
 
 int main()
 {
-	int n, e, start, end;
+	int n, e, param1, param2, result;
 	//cin >> n >> e;
 	n = 4;
 	e = 6;
-	start = 2;
-	end = 3;
+	param1 = 2;
+	param2 = 3;
+	result = 0;
 
 	linkList.resize(n + 1);
 	visit.resize(n + 1);
 	distances.resize(n + 1);
 
-	for (int i = 1; i < n + 1; i++)
-		distances[i] = INT_MAX;
+	init(n);
 
 	/*for (int i = 0; i < e; i++)
 	{
@@ -83,11 +92,33 @@ int main()
 	linkList[1].push_back({ 4,4 });
 	linkList[4].push_back({ 1,4 });
 
-	visit[start] = true;
-	explorer(start, 0);
+	visit[1] = true;
+	distances[1] = 0;
+	explorer(1, 0);
 
-	if (visit[end] == true)
-		cout << distances[end];
+	if (distances[param1] < distances[param2])
+	{
+		result += distances[param1];
+		init(n);
+		explorer(param1, 0);
+		result += distances[param2];
+		init(n);
+		explorer(param2, 0);
+	}
+	else
+	{
+		result += distances[param2];
+		init(n);
+		explorer(param2, 0);
+		result += distances[param1];
+		init(n);
+		explorer(param1, 0);
+	}
+
+	result += distances[n];
+
+	if (visit[n] == true)
+		cout << result;
 	else
 		cout << -1;
 }
