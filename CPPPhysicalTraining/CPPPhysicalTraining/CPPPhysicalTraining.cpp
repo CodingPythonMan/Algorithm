af842a7b165095f4eaa314ee7ac6c366f1a1c71b
JuYoung Kim <robolab1902@naver.com>
2023-03-14 19:01:18 +0900

    교과서대로 Dijkstra 진행 중

--- CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
+++ CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
@@ -15,14 +15,13 @@ int main()
 
 	distances.resize(n);
 	visibles.resize(n);
+	linkList.resize(n);
 
 	for (int i = 1; i < n; i++)
 		distances[i] = LONG_MAX;
 
 	for (int i = 0; i < n; i++)
-	{
 		cin >> visibles[i];
-	}
 	
 	for (int i = 0; i < e; i++)
 	{
@@ -32,4 +31,18 @@ int main()
 		linkList[b].push_back({ a,c });
 	}
 
+	auto cmp = [](const pair<int, long>& p1, const pair<int, long>& p2) {
+		return (p1.second > p2.second);
+	};
+
+	priority_queue<pair<int, long>, vector<pair<int, long>>, decltype(cmp)> pq(cmp);
+
+	//  Q 에 G의 모든 정점들을 넣는다.
+	while (pq.size() != 0)
+	{
+		const pair<int, long>& node = pq.top();
+		pq.pop();
+
+		// node 에 대한 인접 정점 접근
+	}
 }
