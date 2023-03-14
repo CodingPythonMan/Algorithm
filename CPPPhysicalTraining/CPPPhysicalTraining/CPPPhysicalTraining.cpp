960e8bdba0c051e0a8285f0a04eed014100be6e4
JuYoung Kim <robolab1902@naver.com>
2023-03-14 12:23:59 +0900

    init 에 제대로 추가

--- CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
+++ CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
@@ -13,13 +13,16 @@ vector<vector<vertex>> linkList;
 vector<bool> visit;
 vector<int> distances;
 
-void init(int n)
+void init(int start, int n)
 {
 	for (int i = 1; i < n + 1; i++)
 		distances[i] = INT_MAX;
 
 	for (int i = 1; i < n + 1; i++)
 		visit[i] = false;
+
+	visit[start] = true;
+	distances[start] = 0;
 }
 
 void dijkstra(const vertex& ver)
@@ -42,10 +45,7 @@ void dijkstra(const vertex& ver)
 			distances[v.index] = v.weight + ver.weight;
 
 		if (linkList[v.index].empty() == false)
-		{
 			pq.push({ v.index, distances[v.index] });
-		}
-			
 	}
 
 	if (pq.size() != 0)
@@ -60,15 +60,13 @@ int explorer(int param1, int param2, int n)
 {
 	int result = 0;
 
-	init(n);
-	visit[1] = true;
-	distances[1] = 0;
+	init(1, n);
 	dijkstra({ 1,0 });
 	result += distances[param1];
-	init(n);
+	init(param1, n);
 	dijkstra({ param1, 0 });
 	result += distances[param2];
-	init(n);
+	init(param2, n);
 	dijkstra({ param2, 0 });
 	result += distances[n];
 
@@ -78,37 +76,17 @@ int explorer(int param1, int param2, int n)
 int main()
 {
 	int n, e, param1, param2;
-	//cin >> n >> e;
-	n = 4;
-	e = 6;
+	n = 2;
+	e = 2;
 	param1 = 2;
-	param2 = 3;
+	param2 = 1;
 
 	linkList.resize(n + 1);
 	visit.resize(n + 1);
 	distances.resize(n + 1);
 
-	init(n);
-
-	/*for (int i = 0; i < e; i++)
-	{
-		int a, b, c;
-		cin >> a >> b >> c;
-		linkList[a].push_back({ b,c });
-	}
-	cin >> start >> end;*/
 	linkList[1].push_back({ 2,3 });
 	linkList[2].push_back({ 1,3 });
-	linkList[2].push_back({ 3,3 });
-	linkList[3].push_back({ 2,3 });
-	linkList[3].push_back({ 4,1 });
-	linkList[4].push_back({ 3,1 });
-	linkList[1].push_back({ 3,5 });
-	linkList[3].push_back({ 1,5 });
-	linkList[2].push_back({ 4,5 });
-	linkList[4].push_back({ 2,5 });
-	linkList[1].push_back({ 4,4 });
-	linkList[4].push_back({ 1,4 });
 
 	int result = explorer(param1, param2, n);
 	int result2 = explorer(param2, param1, n);
