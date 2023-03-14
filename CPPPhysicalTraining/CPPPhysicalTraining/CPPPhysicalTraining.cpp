59e2f644f253fe46c0e79d82767a9417e4d933a1
JuYoung Kim <robolab1902@naver.com>
2023-03-14 18:43:20 +0900

    17396 백도어 문제 푸는 중

--- CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
+++ CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
@@ -4,69 +4,26 @@
 #include <iostream>
 using namespace std;
 
-struct node
-{
-	int index;
-	int weight;
-};
-
-vector<vector<node>> linkList;
-vector<bool> visit;
-vector<int> distances;
-
-void explorer(const node& vertex)
-{
-	vector<node>& links = linkList[vertex.index];
-
-	auto cmp = [](const node& v1, const node& v2) {
-		return (v1.weight > v2.weight);
-	};
-
-	priority_queue<node, vector<node>, decltype(cmp)> pq(cmp);
-
-	for (int i = 0; i < static_cast<int>(links.size()); i++)
-	{
-		const node& v = links[i];
-
-		if (visit[v.index] == true)
-			continue;
-
-		if (distances[v.index] > vertex.weight + v.weight)
-			distances[v.index] = vertex.weight + v.weight;
-
-		if (linkList[v.index].empty() == false)
-			pq.push({ v.index, distances[v.index] });
-	}
-
-	if (pq.size() != 0)
-	{
-		const node& v = pq.top();
-		visit[v.index] = true;
-		explorer(v);
-	}
-}
+vector<long> distances;
+vector<int> visibles;
+vector<vector<pair<int, long>>> linkList;
 
 int main()
 {
 	int n, e;
 	cin >> n >> e;
 
-	linkList.resize(n);
-	visit.resize(n);
 	distances.resize(n);
+	visibles.resize(n);
 
 	for (int i = 1; i < n; i++)
-		distances[i] = INT_MAX;
+		distances[i] = LONG_MAX;
 
 	for (int i = 0; i < n; i++)
 	{
-		int visible;
-		cin >> visible;
-		if (visible == 1)
-			visit[i] = true;
+		cin >> visibles[i];
 	}
-	visit[n - 1] = false;
-
+	
 	for (int i = 0; i < e; i++)
 	{
 		int a, b, c;
@@ -75,11 +32,4 @@ int main()
 		linkList[b].push_back({ a,c });
 	}
 
-	visit[0] = true;
-	explorer({ 0,0 });
-
-	if (distances[n - 1] == INT_MAX)
-		cout << -1;
-	else
-		cout << distances[n - 1];
 }
