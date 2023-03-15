b75aec85b699a95ce7258fe734216ff93f285606
JuYoung Kim <robolab1902@naver.com>
2023-03-15 15:12:07 +0900

    백도어 17396 문제 풀어 냄. Dijkstra 를 조금 이상하게 풀고 있었음

--- CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
+++ CPPPhysicalTraining/CPPPhysicalTraining/CPPPhysicalTraining.cpp
@@ -6,7 +6,37 @@ using namespace std;
 
 vector<long> distances;
 vector<int> visibles;
-vector<vector<pair<int, long>>> linkList;
+vector<vector<pair<int, long>>> graph;
+
+void explorer(int start)
+{
+	auto cmp = [](const pair<int, long>& p1, const pair<int, long>& p2) {
+		return (p1.second > p2.second);
+	};
+
+	priority_queue<pair<int, long>, vector<pair<int, long>>, decltype(cmp)> pq(cmp);
+	distances[start] = 0;
+	pq.push({ start, 0 });
+
+	//  Q 에 G의 모든 정점들을 넣는다.
+	while (pq.size() != 0)
+	{
+		const pair<int, long> node = pq.top();
+		pq.pop();
+
+		if (node.second > distances[node.first])
+			continue;
+
+		for (auto& next : graph[node.first])
+		{
+			if (visibles[next.first] == 0 && distances[next.first] > node.second + next.second)
+			{
+				distances[next.first] = node.second + next.second;
+				pq.push({ next.first, distances[next.first] });
+			}
+		}
+	}
+}
 
 int main()
 {
@@ -15,34 +45,27 @@ int main()
 
 	distances.resize(n);
 	visibles.resize(n);
-	linkList.resize(n);
+	graph.resize(n);
 
 	for (int i = 1; i < n; i++)
 		distances[i] = LONG_MAX;
 
 	for (int i = 0; i < n; i++)
 		cin >> visibles[i];
+	visibles[n - 1] = 0;
 	
 	for (int i = 0; i < e; i++)
 	{
 		int a, b, c;
 		cin >> a >> b >> c;
-		linkList[a].push_back({ b,c });
-		linkList[b].push_back({ a,c });
+		graph[a].push_back({ b,c });
+		graph[b].push_back({ a,c });
 	}
 
-	auto cmp = [](const pair<int, long>& p1, const pair<int, long>& p2) {
-		return (p1.second > p2.second);
-	};
+	explorer(0);
 
-	priority_queue<pair<int, long>, vector<pair<int, long>>, decltype(cmp)> pq(cmp);
-
-	//  Q 에 G의 모든 정점들을 넣는다.
-	while (pq.size() != 0)
-	{
-		const pair<int, long>& node = pq.top();
-		pq.pop();
-
-		// node 에 대한 인접 정점 접근
-	}
+	if (distances[n - 1] == LONG_MAX)
+		cout << -1;
+	else
+		cout << distances[n - 1];
 }
