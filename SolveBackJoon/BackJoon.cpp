977acb0904494783069fdaad17de79cf7043014c
CodingPythonMan <robolab1902@naver.com>
2023-10-15 16:49:40 +0900

    쇼 문제 풀다가 안되었다. 마저 풀기 바란다

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -3,34 +3,5 @@ using namespace std;
 
 int main()
 {
-	while (1)
-	{
-		string str;
-		cin >> str;
-
-		if (str.compare("0") == 0)
-			break;
-
-		bool answer = false;
-
-		string cmpStr = str;
-		for (size_t i = 0; i < str.length(); i++)
-		{
-			cmpStr[str.length()-1 - i] = str[i];
-		}
-
-		if (cmpStr.compare(str) == 0)
-			answer = true;
-		else
-			answer = false;
-
-		if (answer)
-		{
-			cout << "yes" << "\n";
-		}
-		else
-		{
-			cout << "no" << "\n";
-		}
-	}
+	
 }
