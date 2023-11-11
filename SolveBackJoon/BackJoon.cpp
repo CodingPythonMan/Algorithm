d42794a832a465af2ccda2c729876ceead35963d
CodingPythonMan <robolab1902@naver.com>
2023-11-11 19:42:40 +0900

    클래스 객체 정렬을 위해 퀵소트 써보고있는데 생각보다 잘 안되네... 계속 노력이 필요할 것 같다.

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,36 +1,85 @@
 ﻿#include <iostream>
 using namespace std;
 
+struct Client
+{
+	int age;
+	char name[100];
+	int order;
+};
+
+int order = 0;
+
+void QuickSort(Client* clients, int left, int right)
+{
+	while (left < right)
+	{
+		Client* pivot = &clients[left];
+		int low = left + 1;
+		int high = right;
+
+		while (low <= high)
+		{
+			while (pivot->age >= clients[low].age)
+			{
+				if (pivot->age == clients[low].age && pivot->order >= clients[low].order)
+				{
+					break;
+				}
+
+				low++;
+			}
+
+			while (pivot->age <= clients[high].age)
+			{
+				if (pivot->age == clients[high].age && pivot->order <= clients[high].order)
+				{
+					break;
+				}
+
+				high--;
+			}
+
+			if (low <= high)
+			{
+				Client temp = clients[low];
+				clients[low] = clients[high];
+				clients[high] = temp;
+			}
+		}
+
+		Client temp = *pivot;
+		*pivot = clients[high];
+		clients[high] = temp;
+
+		QuickSort(clients, left, high - 1);
+		QuickSort(clients, high + 1, right);
+	}
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	while (1)
+	int n;
+	cin >> n;
+	Client* clients = new Client[n];
+
+	for (int i = 0; i < n; i++)
 	{
-		int a, b, c;
-
-		cin >> a >> b >> c;
-		
-		if (a == 0 && b == 0 && c == 0)
-			break;
-
-		if (a * a == b * b + c * c)
-		{
-			cout << "right\n";
-			continue;
-		}
-		else if (c * c == a * a + b * b)
-		{
-			cout << "right\n";
-			continue;
-		}
-		else if (b * b == a * a + c * c)
-		{
-			cout << "right\n";
-			continue;
-		}
-
-		cout << "wrong\n";
+		cin >> clients[i].age;
+		cin >> clients[i].name;
+		clients[i].order = order;
+		order++;
 	}
+
+	QuickSort(clients, 0, n-1);
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << clients[i].age << clients[i].name << "\n";
+	}
+
+	delete[] clients;
 }
