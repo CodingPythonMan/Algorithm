983bd3c6cc396fa6b3c99cc35c2734ca6ae95dc5
CodingPythonMan <robolab1902@naver.com>
2023-11-11 13:13:18 +0900

    gcd, lcd 구한 문제 커밋 남긴 후, 블랙잭 문제로 전환

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,26 +1,41 @@
 ﻿#include <iostream>
 using namespace std;
 
-int gcd(int a, int b) {
-	int c = a % b;
-	while (c != 0) {
-		a = b;
-		b = c;
-		c = a % b;
-	}
-	return b;
-}
-
-int lcm(int a, int b) {
-	return (a * b) / gcd(a, b);
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n1, n2;
-	cin >> n1 >> n2;
-	cout << gcd(n1, n2) << "\n" << lcm(n1, n2);
+	int n;
+	int target;
+
+	cin >> n >> target;
+
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	int min = 9999999;
+	int sum = 0;
+	int answer = 0;
+	for (int i = 0; i < n - 2; i++)
+	{
+		for (int j = i + 1; j < n - 1; j++)
+		{
+			for (int k = j + 1; k < n; k++)
+			{
+				sum = arr[i] + arr[j] + arr[k];
+
+				if (target - sum < min && target - sum >= 0)
+				{
+					min = target - sum;
+					answer = sum;
+				}
+			}
+		}
+	}
+
+	cout << answer;
 }
