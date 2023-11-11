b71ee0ca0d1e819ceb72142adeb32015dc4a0937
CodingPythonMan <robolab1902@naver.com>
2023-11-11 17:30:45 +0900

    직각삼각형 구하는 건 너무 쉽다

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -6,36 +6,31 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n;
-	int target;
-
-	cin >> n >> target;
-
-	int* arr = new int[n];
-	for (int i = 0; i < n; i++)
+	while (1)
 	{
-		cin >> arr[i];
-	}
+		int a, b, c;
 
-	int min = 9999999;
-	int sum = 0;
-	int answer = 0;
-	for (int i = 0; i < n - 2; i++)
-	{
-		for (int j = i + 1; j < n - 1; j++)
+		cin >> a >> b >> c;
+		
+		if (a == 0 && b == 0 && c == 0)
+			break;
+
+		if (a * a == b * b + c * c)
 		{
-			for (int k = j + 1; k < n; k++)
-			{
-				sum = arr[i] + arr[j] + arr[k];
-
-				if (target - sum < min && target - sum >= 0)
-				{
-					min = target - sum;
-					answer = sum;
-				}
-			}
+			cout << "right\n";
+			continue;
 		}
-	}
+		else if (c * c == a * a + b * b)
+		{
+			cout << "right\n";
+			continue;
+		}
+		else if (b * b == a * a + c * c)
+		{
+			cout << "right\n";
+			continue;
+		}
 
-	cout << answer;
+		cout << "wrong\n";
+	}
 }
