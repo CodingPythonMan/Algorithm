e0625dad239b375e2c5b340f705281422f2d2453
JuYoung Kim <robolab1902@naver.com>
2023-12-15 20:57:42 +0900

    1676 풀었는데 꽤 생각했지만 이만한 풀이방법이 떠오르지 않았다... 생각보다 요즘 생각하는게 힘든가보다

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,83 +1,14 @@
 ﻿#include <iostream>
-#include <algorithm>
 using namespace std;
-
-struct Pos {
-public:
-	int _x;
-	int _y;
-};
-
-/*
-void SwapPos(Pos& pos1, Pos& pos2)
-{
-	Pos pos = pos1;
-	pos1 = pos2;
-	pos2 = pos;
-}
-
-bool ComparePos(const Pos& pos1, const Pos& pos2)
-{
-	if (pos1._x < pos2._x)
-		return true;
-	else if (pos1._x == pos2._x)
-	{
-		return pos1._y < pos2._y;
-	}
-	else
-		return false;
-}
-
-void QuickSort(Pos* posArray, int left, int right)
-{
-	if (left >= right)
-		return;
-
-	int i, j;
-	i = left;
-	j = left;
-
-	Pos& pivot = posArray[right];
-	for (; j < right; j++)
-	{
-		if (ComparePos(posArray[j], pivot))
-		{
-			SwapPos(posArray[i], posArray[j]);
-			i++;
-		}
-	}
-	SwapPos(posArray[i], pivot);
-	
-	QuickSort(posArray, left, i-1);
-	QuickSort(posArray, i+1, right);
-}*/
 
 int main()
 {
 	int n;
+
 	cin >> n;
+	int five = n / 5;
+	int tfive = n / 25;
+	int hfive = n / 125;
 
-	Pos* posArray = new Pos[n];
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> posArray[i]._x >> posArray[i]._y;
-	}
-
-	//QuickSort(posArray, 0, n - 1);
-
-	sort(posArray, posArray + n, [](const Pos& pos1, const Pos& pos2)
-	{
-		if (pos1._x < pos2._x)
-			return true;
-		else if (pos1._x == pos2._x)
-		{
-			return pos1._y < pos2._y;
-		}
-		else
-			return false;
-	});
-
-	for (int i = 0; i < n; i++)
-		cout << posArray[i]._x << " " << posArray[i]._y << "\n";
+	cout << five + tfive + hfive;
 }
