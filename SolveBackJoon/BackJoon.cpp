0f515f76f936848c98e7c8e0a34dfa29e99eb3ee
robo <robolab1902@naver.com>
2023-12-24 00:37:07 +0900

    달팽이 올라가는 문제였는데 충분히 생각하고 푸니까 공식이 내 말대로 설명이 되었다. 이 깨달음이 중요하다. 그냥 될 것 같다하고 넘어가지 말자.

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -3,42 +3,15 @@ using namespace std;
 
 int main()
 {
-	int T;
-	cin >> T;
+	int A, B, V;
 
-	int k, n;
-	for (int i = 0; i < T; i++)
-	{
-		cin >> k >> n;
-		int** secArr = new int*[k + 1];
-		for (int j = 0; j < k + 1; j++)
-		{
-			secArr[j] = new int[n + 1];
-		}
+	cin >> A >> B >> V;
 
-		for (int ho = 0; ho < n + 1; ho++)
-		{
-			secArr[0][ho] = ho;
-		}
+	int dayWork = A - B;
+	int days = (V - A) / dayWork + 1;
 
-		for (int floor = 1; floor < k + 1; floor++)
-		{
-			secArr[floor][1] = 1;
-			secArr[floor][0] = 0;
-		}
+	if ((V - A) % dayWork != 0)
+		days += 1;
 
-		for (int j = 1; j < k + 1; j++)
-		{
-			for (int ho = 1; ho < n + 1; ho++)
-			{
-				secArr[j][ho] = secArr[j-1][ho] + secArr[j][ho-1];
-			}
-		}
-
-		cout << secArr[k][n] << "\n";
-		for (int j = 0; j < k; j++)
-			delete[] secArr[j];
-
-		delete[] secArr;
-	}
+	cout << days << "\n";
 }
