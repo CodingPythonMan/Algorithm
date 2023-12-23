d578acea3450199f37178972d2bbe659b15543c9
robo <robolab1902@naver.com>
2023-12-23 13:01:12 +0900

    2231 풀었다. 쉬웠는데 굳이 메모리에 자료를 저장할 필요 없었다. 그냥 자릿수로 구하는 문제였다.

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,64 +1,39 @@
 ﻿#include <iostream>
-#include <vector>
 using namespace std;
 
-struct Doc
+int Constructor(int num)
 {
-	int Order;
-	int Data;
-};
+	int value = num;
+	for (int i = 0; value > 0; i++)
+	{
+		num += (value % 10);
+		value /= 10;
+	}
+	
+	return num;
+}
 
 int main()
 {
-	int test;
-	cin >> test;
-	vector<Doc> Queue;
-	for (int i = 0; i < test; i++)
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++)
 	{
-		int n, m;
-		cin >> n >> m;
-
-		int num;
-		for (int j = 0; j < n; j++)
-		{
-			cin >> num;
-			Doc doc;
-			doc.Order = j;
-			doc.Data = num;
-			Queue.push_back(doc);
-		}
-
-		Doc front;
-		int loop = 0;
-		while (1)
-		{
-			front = Queue[0];
-
-			bool right = false;
-			for (int j = 0; j < Queue.size(); j++)
-			{
-				if (front.Data < Queue[j].Data)
-				{
-					Queue.erase(Queue.begin());
-					Queue.push_back(front);
-					right = true;
-					break;
-				}
-			}
-
-			if (right)
-				continue;
-
-			Queue.erase(Queue.begin());
-			loop++;
-
-			if (m == front.Order)
-			{
-				break;
-			}
-		}
-
-		Queue.clear();
-		cout << loop << "\n";
+		arr[i] = Constructor(i);
 	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == n)
+		{
+			cout << i << "\n";
+			delete[] arr;
+			return 0;
+		}
+	}
+
+	cout << 0 << "\n";
+
+	delete[] arr;
 }
