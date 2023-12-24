0977ed05c757677718afddc320e37e1762bf7272
robo <robolab1902@naver.com>
2023-12-24 15:44:30 +0900

    getline 으로 한줄씩 읽는거 문자열 문제 풀 때 아주 유용할듯 싶다

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,17 +1,58 @@
 ﻿#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
 int main()
 {
-	int A, B, V;
+	stack<char> Stack;
+	bool answer;
 
-	cin >> A >> B >> V;
+	while (1)
+	{
+		answer = true;
+		string line;
+		getline(cin, line);
 
-	int dayWork = A - B;
-	int days = (V - A) / dayWork + 1;
+		if (line[0] == '.')
+			break;
+		
+		for(int i=0; line[i] != '.'; i++)
+		{
+			if (line[i] == ']' || line[i] == ')')
+			{
+				if (Stack.size() == 0)
+				{
+					answer = false;
+					break;
+				}
+				else
+				{
+					char Pop = Stack.top();
+					if (line[i] == ']' && Pop != '['
+					|| line[i] == ')' && Pop != '(')
+					{
+						answer = false;
+						break;
+					}
+					Stack.pop();
+					continue;
+				}
+			}
 
-	if ((V - A) % dayWork != 0)
-		days += 1;
+			if (line[i] == '(' || line[i] == '[')
+				Stack.push(line[i]);
+		}
 
-	cout << days << "\n";
+		if (Stack.size() > 0)
+			answer = false;
+
+		if(answer == true)
+			cout << "yes" << "\n";
+		else
+			cout << "no" << "\n";
+			
+		while (false == Stack.empty()) 
+			Stack.pop();
+	}
 }
