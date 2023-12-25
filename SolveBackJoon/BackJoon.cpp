1b14415819c8c1b94150ed7914d52999d0e912f9
robo <robolab1902@naver.com>
2023-12-25 09:17:59 +0900

    7568 덩치. 문제를 정말 잘 읽어야하는 문제이다.

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,58 +1,41 @@
 ﻿#include <iostream>
-#include <stack>
-#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+struct Pos {
+	int X;
+	int Y;
+	int Rank;
+};
 
 int main()
 {
-	stack<char> Stack;
-	bool answer;
+	int n;
+	cin >> n;
 
-	while (1)
+	vector<Pos> Infos;
+	for (int i = 0; i < n; i++)
 	{
-		answer = true;
-		string line;
-		getline(cin, line);
+		Pos Info;
+		Info.Rank = 1;
+		cin >> Info.X >> Info.Y;
+		Infos.push_back(Info);
+	}
 
-		if (line[0] == '.')
-			break;
-		
-		for(int i=0; line[i] != '.'; i++)
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
 		{
-			if (line[i] == ']' || line[i] == ')')
+			if (Infos[i].X > Infos[j].X && Infos[i].Y > Infos[j].Y)
 			{
-				if (Stack.size() == 0)
-				{
-					answer = false;
-					break;
-				}
-				else
-				{
-					char Pop = Stack.top();
-					if (line[i] == ']' && Pop != '['
-					|| line[i] == ')' && Pop != '(')
-					{
-						answer = false;
-						break;
-					}
-					Stack.pop();
-					continue;
-				}
+				Infos[j].Rank++;
 			}
-
-			if (line[i] == '(' || line[i] == '[')
-				Stack.push(line[i]);
 		}
+	}
 
-		if (Stack.size() > 0)
-			answer = false;
-
-		if(answer == true)
-			cout << "yes" << "\n";
-		else
-			cout << "no" << "\n";
-			
-		while (false == Stack.empty()) 
-			Stack.pop();
+	for (int i = 0; i < n; i++)
+	{
+		cout << Infos[i].Rank << " ";
 	}
 }
