0f21e85dd79e5fa5e3599dfff6ffed0d74401a67
robo <robolab1902@naver.com>
2023-12-29 23:24:07 +0900

    케빈 1389 베이컨 문제 어떻게 푸는게 좋을까?... 오랜만에 푸는 경로 몇 개 구하는 문제라 꽤 어렵다. 천천히 풀어보자.

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,84 +1,26 @@
 ﻿#include <iostream>
-#include <cmath>
 using namespace std;
-
-int N, M, B;
-int** Map;
-double sum = 0;
-int MapLength ;
-
-int GetTime(int Aver)
-{
-	int TimeSum = 0;
-	int differ;
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (Map[i][j] < Aver)
-			{
-				differ = Aver - Map[i][j];
-				TimeSum += differ;
-			}
-			else if (Map[i][j] > Aver)
-			{
-				differ = Map[i][j] - Aver;
-				TimeSum += (2 * differ);
-			}
-		}
-	}
-	
-	return TimeSum;
-}
 
 int main()
 {
-	cin >> N >> M >> B;
+	int n, m;
+	cin >> n >> m;
 
-	Map = new int*[N];
-	for (int i = 0; i < N; i++)
+	char** route = new char*[n];
+	for (int i = 0; i < n; i++)
 	{
-		Map[i] = new int[M];
+		route[i] = new char[n];
 	}
 
-	MapLength = M * N;
-	int num;
-	for (int i = 0; i < N; i++)
+	int Start, End;
+	for (int i = 0; i < m; i++)
 	{
-		for (int j = 0; j < M; j++)
-		{
-			cin >> num;
-			Map[i][j] = num;
-			sum += num;
-		}
+		cin >> Start >> End;
+		route[Start - 1][End - 1] = 1;
+
+		
 	}
-
-	// 평균 낸 뒤 두 개 방향 조사.
-	int Aver = (int)floor(sum / MapLength);
-	int TimeSum = 0;
-	int beforeTime = 0;
-	int index = 0;
-
-	TimeSum = GetTime(Aver);
-	beforeTime = TimeSum;
-	index++;
-	while (sum + B >= (Aver+index) * MapLength)
-	{
-		TimeSum = GetTime(Aver + index);
-		if (beforeTime < TimeSum)
-		{
-			TimeSum = beforeTime;
-			break;
-		}
-		beforeTime = TimeSum;
-		index++;
-	}
-
-	cout << TimeSum << " " << Aver + index - 1 << "\n";
-
-	for (int i = 0; i < N; i++)
-		delete[] Map[i];
-
-	delete[] Map;
+	
+	// 어떻게 구해볼까?
+	// 최소 거리를 어떻게 구하지? 
 }
