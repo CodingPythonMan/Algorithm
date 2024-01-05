c9ba955ac1cbf29110d8992a1228c2877728d34e
JuYoung Kim <robolab1902@naver.com>
2024-01-05 18:59:02 +0900

    백준 문제 1107 풀고 있다. 현재 N을 자릿수로 나누는 중

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 
 int main() 
 {
-	
+	int n;
+	cin >> n;
+
+	int Ban, btn;
+	cin >> Ban;
+	vector<int> BanBtn;
+	for (int i = 0; i < Ban; i++)
+	{
+		cin >> btn;
+		BanBtn.push_back(btn);
+	}
+
+	vector<int> Pos;
+	for (int i = 0; i < n; i++)
+	{
+		
+	}
 }
