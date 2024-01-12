021b348474aeb49f1e45063f58bfda12addb262b
JuYoung Kim <robolab1902@naver.com>
2024-01-12 20:57:36 +0900

    백준 문제는 풀면 풀수록 새로운 걸 발견하는 것 같다.

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -1,47 +1,24 @@
-﻿#include <string>
+﻿#include <iostream>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-#include <cmath>
 using namespace std;
 
-vector<int> dp;
-bool btnList[10] = { false };
-
-bool check(int now) 
+int main() 
 {
-	string target = to_string(now);
-	for (int i = 0; i < target.length(); i++)
+	string sen;
+	cin >> sen;
+
+	// 50글자 이내
+	vector<char> oper;
+	vector<int> num;
+	int numCount = 0;
+	for (int i = 0; i < sen.length(); i++)
 	{
-		if (true == btnList[target[i] - 48])
-			return false;
-	}
-	return true;
-}
-
-int main() {
-	int n, banBtnCount;
-	cin >> n >> banBtnCount;
-
-	int ban;
-
-	for (int i = 0; i < banBtnCount; i++) 
-	{
-		cin >> ban;
-		btnList[ban] = true;
-	}
-
-	int minimum = abs(n - 100);
-	for (int i = 0; i <= 1000000; i++) 
-	{
-		if (true == check(i)) 
+		if (sen[i] == '+' || sen[i] == '-')
 		{
-			int tmp = abs(n - i) + (int)(to_string(i).length());
-			minimum = min(minimum, tmp);
+			numCount = i;
+
 		}
 	}
 
-	cout << minimum << endl;
-
-	return 0;
+	cout << sen;
 }
