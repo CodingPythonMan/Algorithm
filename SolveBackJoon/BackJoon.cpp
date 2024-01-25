0c0196f212f317f37641cf20a59550bb1d472f8c
JuYoung Kim <robolab1902@naver.com>
2024-01-25 17:51:57 +0900

    그래 백준도 깔끔히 잘 풀고 있다. 이런 문제도 깔끔히 맞는게 중요하다

--- SolveBackJoon/BackJoon.cpp
+++ SolveBackJoon/BackJoon.cpp
@@ -7,18 +7,29 @@ int main()
 	string sen;
 	cin >> sen;
 
+	int answer = 0;
+
 	// 50글자 이내
-	vector<char> oper;
-	vector<int> num;
 	int numCount = 0;
+	int value;
+	bool minusFlag = false;
 	for (int i = 0; i < sen.length(); i++)
 	{
-		if (sen[i] == '+' || sen[i] == '-')
-		{
-			atoi(sen.substr(numCount, i - 1));
-			numCount = i;
-		}
+		if (sen[i] != '+' && sen[i] != '-' && i != sen.length() - 1)
+			continue;
+
+		value = atoi(sen.substr(numCount, i - numCount + 1).c_str());
+		if (i != sen.length() - 1)
+			numCount = i + 1;
+			
+		if (minusFlag == false)
+			answer += value;
+		else
+			answer -= value;
+
+		if (sen[i] == '-')
+			minusFlag = true;
 	}
 
-	cout << sen;
+	cout << answer << "\n";
 }
